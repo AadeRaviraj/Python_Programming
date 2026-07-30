ecf7015c64b0e042baa8a3683b6e6074b301a32d
Raviraj <ravirajaade299@gmail.com>
2026-07-30 21:15:44 +0530

    Updated ML and Python application files

--- Machine_Learning/Variance_Standard_Deviation.py
+++ Machine_Learning/Variance_Standard_Deviation.py
@@ -7,3 +7,5 @@ data1 = [28, 29, 30, 31, 32]
 data2 = [10, 20, 30, 40, 50]
 
 print("Mean data1 :", np.mean(data1))
+print("Variance Data : ", np.var(data1))
+print("Standard deviation :", np.std(data1))
